a20cb17960f58247f635d98cd200aa9e49bb0d9b
Ben Dowen <33220797+dowenb@users.noreply.github.com>
2020-01-13 15:36:27 +0000

    Update weather_fixtures.spec.js
    Extended coverage to multiple days and cities

--- cypress/integration/weather/weather_fixtures.spec.js
+++ cypress/integration/weather/weather_fixtures.spec.js
@@ -2,17 +2,34 @@
 
 /* eslint-env mocha */
 describe('Metaweather API', () => {
-    it('a thing', () =>{
+    it('Tomorrows Weather in Nottingham should return JSON response', () =>{
         cy.fixture('cities').then((cities) =>{
             cy.fixture('days').then((days) =>{
-                cy.log(cities)
-                cy.log(cities.Nottingham)
-                cy.log(days.Tomorrow)
-
                 cy.request(cities.Nottingham + days.Tomorrow)
                 .its('headers')
                 .its('content-type')
                 .should('include', 'application/json')
+            })
+        })
+    })
+
+    it('Interview day in Hull should return 200 OK', () =>{
+        cy.fixture('cities').then((cities) =>{
+            cy.fixture('days').then((days) =>{
+                cy.request(cities.Hull + days.Interview)
+                .its('status')
+                .should('eq', 200)
+            })
+        })
+    })
+
+    it('Yesterdays Weather in Sheffield should return more then one result', () =>{
+        cy.fixture('cities').then((cities) =>{
+            cy.fixture('days').then((days) =>{
+                cy.request(cities.Sheffield + days.Yesterday)
+                .its('body')
+                .its('length')
+                .should('gt', '1')
             })
         })
     })
